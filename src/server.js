d46afbc1865c6debd8b695c23297df641eb3286e
kkkwp <chxrryda@gmail.com>
2023-02-27 10:44:58 +0900

    feat: connect with ws

--- src/server.js
+++ src/server.js
@@ -15,4 +15,11 @@ const handleListen = () => console.log(`Listening on https://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+
+function handleConnection(socket) {
+    console.log(socket);
+}
+
+wss.on("connection", handleConnection);
+
 server.listen(3000, handleListen);
