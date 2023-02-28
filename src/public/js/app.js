e548916a9713bbb6d062f804a575f4f6cefb51e9
kkkwp <chxrryda@gmail.com>
2023-02-28 11:17:51 +0900

    feat: show room names

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,15 +2,32 @@ const socket = io();
 
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-function backendDone(msg) {
-    console.log(`The backend says: `, msg);
+// 방에 입장하기 전에는 방 속성 숨기기
+room.hidden = true;
+
+// 방에 입장하면 실행될 함수
+function showRoom() {
+    // 화면 표시
+    welcome.hidden = true;
+    room.hidden = false;
+    // 방 이름 표시
+    const h3= room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
 }
+
+let roomName;
 
 function handleRoomSubmit(event) {
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", input.value, backendDone);
+    // emit: 서버가 현재 접속해있는 모든 클라이언트에게 이벤트 전달
+    // 방에 접속하면, input.value를 전달하고 showRoom 함수 실행
+    socket.emit("enter_room", input.value, showRoom);
+    // 방 이름 가져오기
+    roomName = input.value;
+    // input 값 초기화
     input.value = "";
 }
 
--- src/server.js
+++ src/server.js
@@ -4,21 +4,30 @@ import express from "express";
 
 const app = express();
 
+// front 설정
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+// redirect
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
+// httpServer에 socketIO를 같이 올림
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+// 연결되었을 때의 이벤트
 wsServer.on("connection", (socket) => {
+    // onAny: 미들웨어처럼, 모든 event에서 다음의 함수를 수행
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+    });
+
+    // on: 방에 입장했을 때의 이벤트
     socket.on("enter_room", (roomName, done) => {
-        console.log(roomName);
-        setTimeout(() => {
-            done("hello from the backend"); 
-        }, 15000);
+        // 방에 들어감
+        socket.join(roomName);
+        done(); 
     });
 });
 
